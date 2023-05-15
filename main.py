b769563153781e51a89d75e499339c59ee365f8e
Adam Abdallah <118904631+compoundevents@users.noreply.github.com>
2023-05-15 21:33:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+# Importing pandas moudel.
 import pandas
-
+# Reading the nato_phonetic_alphabet.csv and saving it into a variable called data.
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
-
+# Creating new dictionary using the dictionary comprehension.
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-# print(phonetic_dict)
-
+# Taking input from user about the word and uppercase it.
 word = input("Enter a word: ").upper()
+# creating a list using list comprehension.
 output_list = [phonetic_dict[letter] for letter in word]
 print(output_list)
